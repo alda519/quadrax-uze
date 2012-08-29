606aa2fbdff8cbffcef41dd8d9a9cc30fe3203d1
alda519 <galda519@gmail.com>
2012-08-29 12:29:54 +0200

    wip

--- mapeditor.c
+++ mapeditor.c
@@ -19,7 +19,7 @@
 #define SCALE_Y 3
 
 #define PAL_WIDTH 10 * 6 * SCALE_X
-#define PAL_HEIGHT 8 * 6 * SCALE_Y
+#define PAL_HEIGHT 6 * 8 * SCALE_Y * 2
 
 #define MAP_WIDTH 40 * 6 * SCALE_X
 #define MAP_HEIGHT 28 * 8 * SCALE_Y
@@ -33,22 +33,50 @@
 
 const char datafile[] = "data/tiles_big.png";
 
-
+// text representation of objects
 enum {
     BLANK = ' ',
     WALL = '#',
+    BLUE_PLAYER = '1',
+    RED_PLAYER = '2',
+    BOULDER = 'B',
+    FINISH = 'F',
 };
+
+// drawing modes
+enum {
+    POINT,
+    RECTANGLE,
+};
+
+// tools pallete
+enum {
+    P_BLANK,
+    P_WALL,
+    P_FINISH,
+    P_BOULDER,
+    P_BLUE_PLAYER,
+    P_RED_PLAYER,
+};
+
+// implicit drawing tools
+int tools[2] = {P_WALL, P_BLANK};
+
 
 // array to store map
 unsigned char map[WIDTH][HEIGHT];
 
-
+/**
+ *
+ */
 int in_map_area(int x, int y)
 {
     return x > 10 && x < 10 + MAP_WIDTH && y > 10 && y < 10 + MAP_HEIGHT;
 }
 
-
+/**
+ *
+ */
 int in_pallete_area(int x, int y)
 {
     int neco = 100;
@@ -56,18 +84,20 @@ int in_pallete_area(int x, int y)
 }
 
 
-int tools[2] = {1, 0};
-
-void change_tool(int button)
+/**
+ *
+ */
+void change_tool(SDL_MouseButtonEvent button)
 {
-    switch(button) {
+    switch(button.button) {
         case SDL_BUTTON_LEFT:
-            tools[0] = !tools[0];
+            tools[0] = (button.y - 110) / 48;
             break;
         case SDL_BUTTON_RIGHT:
-            tools[1] = !tools[1];
+            tools[1] = (button.y - 110) / 48;
             break;
     }
+    printf("tools : %d , %d \n", tools[0], tools[1]);
 }
 
 
@@ -84,7 +114,9 @@ void redraw_grid(SDL_Surface *screen)
     }
 }
 
-
+/**
+ *
+ */
 void init_screen(SDL_Surface *screen)
 {
     // background
@@ -98,21 +130,23 @@ void init_screen(SDL_Surface *screen)
     SDL_Rect preview_area = {.x = MAP_WIDTH + 20, .y = 10, .w = SCALE_X * 6, .h = SCALE_Y * 8 };
     SDL_FillRect(screen, &preview_area, 0x00ff00);
 
+    // draw loaded map
     for(int x = 0; x < WIDTH; ++x) {
         for(int y = 0; y < HEIGHT; ++y) {
             if(map[x][y] == WALL)
-                boxColor(screen, 10+x*BLOCK_X*SCALE_X, 10+y*BLOCK_Y*SCALE_Y,10+(x+1)*BLOCK_X*SCALE_X, 10+(y+1)*BLOCK_Y*SCALE_Y, 0xffff00ff);
+                boxColor(screen, 10+x*BLOCK_X*SCALE_X, 10+y*BLOCK_Y*SCALE_Y,10+(x+1)*BLOCK_X*SCALE_X, 10+(y+1)*BLOCK_Y*SCALE_Y, 0x999999ff);
         }
     }
 
     redraw_grid(screen);
-
     
     // update
     SDL_UpdateRect(screen, 0, 0, 0, 0);
 }
 
-
+/**
+ *
+ */
 void fill_area(SDL_Surface *screen, int x1, int y1, int x2, int y2, int which)
 {
     int tmp;
@@ -129,16 +163,37 @@ void fill_area(SDL_Surface *screen, int x1, int y1, int x2, int y2, int which)
     for(int i = (x1-10)/(BLOCK_X * SCALE_X); i <= (x2-10)/(BLOCK_X*SCALE_X); ++i) {
         for(int j = (y1-10)/(BLOCK_Y*SCALE_Y); j <= (y2-10)/(BLOCK_Y*SCALE_Y); ++j) {
             if(which) {
-                boxColor(screen, 10+i*BLOCK_X * SCALE_X, 10+j*(BLOCK_Y*SCALE_Y), 10+(i+1)*BLOCK_X * SCALE_X, 10+(j+1)*(BLOCK_Y*SCALE_Y) , 0x000000ff);
+                boxColor(screen, 10+i*BLOCK_X * SCALE_X+1, 10+j*(BLOCK_Y*SCALE_Y)+1, 10+(i+1)*BLOCK_X * SCALE_X-1, 10+(j+1)*(BLOCK_Y*SCALE_Y)-1 , 0x000000ff);
                 map[i][j]  = '.';
             } else {
-                boxColor(screen, 10+i*BLOCK_X * SCALE_X, 10+j*(BLOCK_Y*SCALE_Y), 10+(i+1)*BLOCK_X * SCALE_X, 10+(j+1)*(BLOCK_Y*SCALE_Y) , 0x999999ff);
+                boxColor(screen, 10+i*BLOCK_X * SCALE_X+1, 10+j*(BLOCK_Y*SCALE_Y)+1, 10+(i+1)*BLOCK_X * SCALE_X-1, 10+(j+1)*(BLOCK_Y*SCALE_Y)-1, 0x999999ff);
                 map[i][j]  = '#';
             }
         }
     }
+    SDL_UpdateRect(screen, 0, 0, 0, 0);
 }
 
+/**
+ *
+ */
+void fill_point(SDL_Surface *screen, int x, int y, int which)
+{
+    int i = (x-10)/(BLOCK_X * SCALE_X);
+    int j = (y-10)/(BLOCK_Y*SCALE_Y);
+    if(which) {
+        if(map[i][j] == '.')
+            return;
+        boxColor(screen, 10+i*BLOCK_X * SCALE_X+1, 10+j*(BLOCK_Y*SCALE_Y)+1, 10+(i+1)*BLOCK_X * SCALE_X-1, 10+(j+1)*(BLOCK_Y*SCALE_Y)-1 , 0x000000ff);
+        map[i][j]  = '.';
+    } else {
+        if(map[i][j] == '#')
+            return;
+        boxColor(screen, 10+i*BLOCK_X * SCALE_X+1, 10+j*(BLOCK_Y*SCALE_Y)+1, 10+(i+1)*BLOCK_X * SCALE_X-1, 10+(j+1)*(BLOCK_Y*SCALE_Y)-1 , 0x999999ff);
+        map[i][j]  = '#';
+    }
+    SDL_UpdateRect(screen, 0, 0, 0, 0);
+}
 
 /**
  * Loads level
@@ -183,7 +238,9 @@ void save(const char * filename)
 }
 
 
-
+/**
+ *
+ */
 int main(int argc, char *argv[])
 {
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -191,7 +248,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    SDL_Surface * pallete = IMG_Load(datafile);
+    SDL_Surface * pallete = IMG_Load("data/pallete.png");
     if(pallete == NULL) {
         fprintf(stderr, "Error loading data file %s\n", datafile);
         return 1;
@@ -225,21 +282,50 @@ int main(int argc, char *argv[])
 
     SDL_MouseButtonEvent click; // first click
     SDL_Event event;
+    Uint8 *keys;
+
+    int mode = POINT;
+
     do {
         SDL_PollEvent(&event);
-        if(event.type == SDL_MOUSEBUTTONDOWN) {
-            click = event.button;
-            if(in_pallete_area(event.button.x, event.button.y))
-                change_tool(event.button.button);
-        } else if (event.type == SDL_MOUSEBUTTONUP){
-            // fill given rectangle
-            if(in_map_area(event.button.x, event.button.y) && in_map_area(click.x, click.y)) {
-                if(event.button.button == SDL_BUTTON_LEFT || event.button.button == SDL_BUTTON_RIGHT) {
-                    fill_area(screen, click.x, click.y, event.button.x, event.button.y, event.button.button == SDL_BUTTON_RIGHT);
-                    redraw_grid(screen);
-                    SDL_UpdateRect(screen, 0, 0, 0, 0);
+
+        switch(event.type) {
+            case SDL_KEYDOWN: // left constroll switches mode
+            case SDL_KEYUP:
+                if(event.key.keysym.sym == SDLK_LCTRL)
+                    mode = event.type == SDL_KEYDOWN; // POINT/RECTANGLE
+                break;
+
+            case SDL_MOUSEMOTION: // fill particular place
+                if(mode == RECTANGLE)
+                    break;
+                if(!in_map_area(event.button.x, event.button.y))
+                    break;
+                if(event.motion.state & SDL_BUTTON(1)) {
+                    fill_point(screen, event.button.x, event.button.y, 0);
+                } else if(event.motion.state & SDL_BUTTON(3)) {
+                    fill_point(screen, event.button.x, event.button.y, 1);
                 }
-            }
+                break;
+
+            case SDL_MOUSEBUTTONDOWN: // first corner of area to fill
+                click = event.button;
+                if(in_pallete_area(event.button.x, event.button.y))
+                    change_tool(event.button);
+                if(mode == POINT && in_map_area(click.x, click.y)) {
+                    fill_area(screen, click.x, click.y, event.button.x, event.button.y, event.button.button == SDL_BUTTON_RIGHT);
+                }
+                    
+                break;
+
+            case SDL_MOUSEBUTTONUP: // the other corner of area to fill
+                if(mode == POINT)
+                    break;
+                if(in_map_area(event.button.x, event.button.y) && in_map_area(click.x, click.y)) {
+                    if(event.button.button == SDL_BUTTON_LEFT || event.button.button == SDL_BUTTON_RIGHT)
+                        fill_area(screen, click.x, click.y, event.button.x, event.button.y, event.button.button == SDL_BUTTON_RIGHT);
+                }
+                break;
         }
         SDL_Delay(1);
     } while(event.type != SDL_QUIT);
